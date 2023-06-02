28dc52b504c8e6cdabf048d31146fee1697699c2
Alexis Toribio <alexistoba21@hotmail.com>
2023-06-02 11:28:27 -0500

    feat(project/01): Add app and styles

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
+const users = [
+  { userName: "midudev", name: "Miguel Ángel Durán", isFollowing: true },
+  { userName: "pheralb", name: "Pablo H.", isFollowing: false },
+  {
+    userName: "alexistoba21@hotmail.com",
+    name: "Miguel Alexis Toribio Barrueta",
+    isFollowing: true,
+  },
+  { userName: "PacoHdezs", name: "Paco Hdez", isFollowing: true },
+  { userName: "TMChein", name: "Tomas.", isFollowing: false },
+];
+
+export function App() {
+  return (
+    <section className="App">
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </section>
+  );
+}
