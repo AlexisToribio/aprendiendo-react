0600eefadb95a9ba9844365e80fead9795af2316
Alexis Toribio <alexistoba21@hotmail.com>
2023-06-08 10:24:03 -0500

    feat(project/05): Add app and styles

---
+++ projects/05-prueba-tecnica/src/App.jsx
@@ -0,0 +1,14 @@
+import { Seeker } from "./components/Seeker.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <header>
+        <Seeker />
+      </header>
+    </div>
+  );
+}
+
+export default App;
