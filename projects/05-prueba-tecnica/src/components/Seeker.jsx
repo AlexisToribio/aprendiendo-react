d479114cb5ee910dcc313f610f8301d8f5bd8d0c
Alexis Toribio <alexistoba21@hotmail.com>
2023-06-08 10:27:37 -0500

    feat(project/05): Add Seeker component

---
+++ projects/05-prueba-tecnica/src/components/Seeker.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Movies } from "./Movies.jsx";
+import { Loader } from "./Loader.jsx";
+import { useMovies } from "../hooks/useMovies.js";
+
+export function Seeker() {
+  const [title, setTitle] = useState("");
+  const { movies, getMovies, loading } = useMovies();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    getMovies({ title });
+  };
+
+  const handleChange = e => {
+    const newTitle = e.target.value;
+    setTitle(newTitle);
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="search">
+          <span>Buscador de películas</span>
+          <input
+            id="search"
+            placeholder="Avengers, Star Wars, The Matrix..."
+            value={title}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Buscar</button>
+      </form>
+      {loading ? <Loader /> : <Movies movies={movies} />}
+    </>
+  );
+}
